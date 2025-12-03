a678155c15c627e1468f3dafde1cb6fdb75442aa
Zhefan Guo <benguo2003103@gmail.com>
2025-12-02 20:39:52 -0500

    test

---
+++ _next/static/US_rJZdZabvwANnA39M6O/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/SDP_team_52_demo_test//_next/:path+",
+        "destination": "/SDP_team_52_demo_test/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
