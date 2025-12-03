246aa67ed18f8fc6e471ed40e85846963317b9b8
Zhefan Guo <benguo2003103@gmail.com>
2025-12-02 20:51:10 -0500

    prep for Vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
   trailingSlash: true,
-  basePath: '/SDP_team_52_demo_test', 
-  assetPrefix: '/SDP_team_52_demo_test/', 
-}
+};
 
-export default nextConfig
+export default nextConfig;
